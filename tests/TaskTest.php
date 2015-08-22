70ea1c461a026766a10d6b2c42d348e6d89d414c
Alex Costa <alexmcosta.dev@gmail.com>
2015-08-22 15:50:48 -0700

    Add boilerplate to task test

---
+++ tests/TaskTest.php
@@ -0,0 +1,31 @@
+<?php
+
+    //Not sure why this is needed further study must be conducted
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    //Routing to classes
+    require_once "src/Task.php";
+    require_once "src/Category.php";
+
+    //connect to DB
+    $server = 'mysql:host=localhost:8889;dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    //Our testing class using phpunit
+    class TaskTest extends PHUnit_Framework_TestCase
+    {
+        //Our teardown so we start with a fresh DB everytime we test.
+        protected function tearDown()
+        {
+            Task::deleteAll();
+            Category::deleteAll();
+        }
+
+        
+    }
+?>
