9728fe73f1ec8a257c937b12878ce11657256021
Alex Costa <alexmcosta.dev@gmail.com>
2015-08-23 16:47:30 -0700

    Add boilerplate to app.php

---
+++ app/app.php
@@ -0,0 +1,21 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost8889;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    
+
+
+?>
